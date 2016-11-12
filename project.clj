00183f918533d7304842db98f083ede07f9510f1
Yehonathan Sharvit <viebel@gmail.com>
2016-11-12 19:19:21 +0200

    version 0.0.2 - conditional load of klipse

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject viebel/codox-klipse-theme "0.0.1"
+(defproject viebel/codox-klipse-theme "0.0.2"
   :description "Klipse theme for codox"
   :url "https://github.com/viebel/codox-klipse-theme")
